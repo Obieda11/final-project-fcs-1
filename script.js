b3f11dd6d109a8cbb5d81febc66149a89a5389cf
Obieda11 <obiedamansour@gmail.com>
2025-04-21 18:32:04 +0000

    register function

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function showTab(name) {
   document.querySelector('.register').classList.remove('active-tab')
   document.querySelector('.' + name).classList.add('active-tab')
 }
+
+function registerUser() {
+    let email = document.querySelector('.register-email').value
+    let pass = document.querySelector('.register-pass').value
+    let users = JSON.parse(localStorage.getItem('users') || '[]')
+    users.push({ email: email, pass: pass, scores: {} })
+    localStorage.setItem('users', JSON.stringify(users))
+    alert('registered')
+  }
